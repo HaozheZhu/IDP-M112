b1b92b34585dc11ec9bdb68272a58e6ae51ebbcc
Haozhe Zhu <hz403@cam.ac.uk>
2022-10-14 12:18:26 +0100

    push

--- a_GLOBAL/c_LOOP.ino
+++ a_GLOBAL/c_LOOP.ino
@@ -1,16 +1,9 @@
 void loop() {
-  if(line_sensor_left==0 && line_sensor_right==0){
-    M1->run(FORWARD);
-    M2->run(FORWARD); 
-    delay(100);  
-    M1->run(RELEASE);
-    M2->run(RELEASE); 
-    delay(100);
-  }
-  else if(line_sensor_left==1 && line_sensor_right==0){
-    steer_left(); 
-  }
-  else if(line_sensor_left==0 && line_sensor_right==1){
-    steer_right(); 
-  }
+  //motor_test_run();   
+  //steer_left(); 
+  M1->setSpeed(100);
+  M2->setSpeed(100);
+  M1->run(FORWARD);
+  M2->run(FORWARD); 
+  delay(2000);
 }
--- a_GLOBAL/d_FUNCTIONS.ino
+++ a_GLOBAL/d_FUNCTIONS.ino
@@ -35,3 +35,32 @@ void steer_left() {
   M1->run(RELEASE); 
   M2->run(RELEASE); 
 } 
+
+void motor_test_run() {
+  M1->run(FORWARD);
+  M2->run(FORWARD); 
+  delay(2000);
+  M1->run(BACKWARD);
+  M2->run(BACKWARD); 
+  delay(2000);
+  M1->run(RELEASE);
+  M2->run(RELEASE); 
+  delay(2000);
+}
+
+void follow_line() {
+    if(line_sensor_left==0 && line_sensor_right==0){
+    M1->run(FORWARD);
+    M2->run(FORWARD); 
+    delay(100);  
+    M1->run(RELEASE);
+    M2->run(RELEASE); 
+    delay(100);
+  }
+  else if(line_sensor_left==1 && line_sensor_right==0){
+    steer_left(); 
+  }
+  else if(line_sensor_left==0 && line_sensor_right==1){
+    steer_right(); 
+  }
+}
