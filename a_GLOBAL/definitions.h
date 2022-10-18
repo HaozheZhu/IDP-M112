d5ffff648f04768aeb3eeb0ed641541764ceda25
LimaBravoVictor <lbv21@cam.ac.uk>
2022-10-18 14:11:06 +0100

    Marked out the servo pins in the header

--- a_GLOBAL/definitions.h
+++ a_GLOBAL/definitions.h
@@ -2,10 +2,12 @@
 #define line_sensor_back 3
 #define line_sensor_left 4
 #define line_sensor_right 5
-
-#define US_trig 9
-#define US_echo 10
-
+//Servo pins foe sheild
+#define servo_pin_one 9
+#define servo pin_two 10 
+// Pins for ultrasound
+#define US_trig 5
+#define US_echo 6
 //Constants for PID controller setup
 #define PID_output_bits 8
 #define PID_output_signed true
@@ -16,5 +18,3 @@
 #define block_approach_proportianal 1.0
 #define block_approach_integral 0.1
 #define block_approach_derivative 0.1
-
-
