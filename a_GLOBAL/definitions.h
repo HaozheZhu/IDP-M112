b9f904cfb8a556b53aa5dc3c4455ee9aa8284595
LimaBravoVictor <lbv21@cam.ac.uk>
2022-10-18 14:43:41 +0100

    Added a placeholder pin for the hall sensor

--- a_GLOBAL/definitions.h
+++ a_GLOBAL/definitions.h
@@ -2,9 +2,10 @@
 #define line_sensor_back 3
 #define line_sensor_left 4
 #define line_sensor_right 5
-//Servo pins foe sheild
+//Servo pins for sheild
 #define servo_pin_one 9
 #define servo pin_two 10 
+#define hall_pin 11 // Temp, here to allow compilation
 // Pins for ultrasound
 #define US_trig 5
 #define US_echo 6
