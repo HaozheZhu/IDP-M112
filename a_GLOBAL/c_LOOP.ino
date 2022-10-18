48cd2618d50f00481ac8dd516cf6f91444c405f2
Haozhe Zhu <hz403@cam.ac.uk>
2022-10-18 10:24:40 +0100

    junction handling

--- a_GLOBAL/c_LOOP.ino
+++ a_GLOBAL/c_LOOP.ino
@@ -1,7 +1,27 @@
 void loop() {
   //motor_test_run(); 
   //Serial.println( hc.dist() ); 
-  follow_line(250); 
+  int line_sensor_left_value = digitalRead(line_sensor_left); 
+  int line_sensor_right_value = digitalRead(line_sensor_right); 
+  int line_sensor_front_value = digitalRead(line_sensor_front); 
+  int line_sensor_back_value = digitalRead(line_sensor_back); 
+
+  if(line_sensor_front_value == 0 && line_sensor_back_value == 1){
+    //going off the line
+    follow_line(250); 
+  }
+  else if(line_sensor_front_value == 1 && line_sensor_back_value == 1){
+    //on the line but at junction
+    if(line_sensor_left_value == 1 && line_sensor_right_value == 0){
+      Serial.println("At left Cross"); 
+      //handle cross
+    }
+    if(line_sensor_right_value == 1 && line_sensor_left_value == 0){
+      Serial.println("At right Cross"); 
+      //handle cross
+    }
+  }
+  
   delay(20); 
   //motor(100, 0, 50); 
 }
--- a_GLOBAL/definitions.h
+++ a_GLOBAL/definitions.h
@@ -2,5 +2,8 @@
 #define line_sensor_back 3
 #define line_sensor_left 4
 #define line_sensor_right 5
-#define US_trig 5
-#define US_echo 6
+
+#define US_trig 9
+#define US_echo 10
+
+#define hall_pin 6
