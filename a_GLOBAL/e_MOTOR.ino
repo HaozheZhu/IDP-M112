524faa252f6461d2751e62112108163c387e884f
LimaBravoVictor <lbv21@cam.ac.uk>
2022-10-22 11:50:41 +0100

    Some changes to motor class structure

--- a_GLOBAL/e_MOTOR.ino
+++ a_GLOBAL/e_MOTOR.ino
@@ -1,6 +1,6 @@
 
 class motor_shield_interface{
-    bool m1_forward, m2_forward, m1_release, m2_release;
+    enum dir{forward, backwards, released}m1_direction, m2_direction;
     byte m1_speed, m2_speed;
     public:
     motor_shield_interface(){
@@ -9,21 +9,21 @@ class motor_shield_interface{
       Adafruit_DCMotor *M2 = AFMS.getMotor(2);
       //set initial
       this->init();
-      m1_forward = true;
-      m2_forward = true;
+      m1_direction = released;
+      m2_direction = released;
       m1_speed =0;
       m2_speed =0;
     }
     void init(){
        if (!AFMS.begin()) {         // create with the default frequency 1.6KHz
-       //if (!AFMS.begin(1000)) {  // OR with a different frequency, say 1KHz
-      Serial.println("Could not find Motor Shield. Check wiring.");
-      while (1);
-    }
+         //if (!AFMS.begin(1000)) {  // OR with a different frequency, say 1KHz
+        Serial.println("Could not find Motor Shield. Check wiring.");
+        while (1);
+      }
       Serial.println("Motor Shield found.");
       M1->run(RELEASE);
-      m1_release = true;
       M2->run(RELEASE);
-      m2_release = true;
+      m1_direction = released;
+      m2_direction = released;
     }
 };
