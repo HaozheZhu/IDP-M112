0c45c312364a5700a13bc2cf312bf84b24e66481
LimaBravoVictor <lbv21@cam.ac.uk>
2022-10-24 16:01:00 +0100

    Block appoarch

--- a_GLOBAL/c_LOOP.ino
+++ a_GLOBAL/c_LOOP.ino
@@ -1,7 +1,13 @@
 void loop() {
 
-  //nav_once(); 
-  //delay(300); 
-  block_approach_line();
+  nav_once(); 
 
+  double dist_front = ultrasound_front(1); 
+  if (dist_front<5.0){
+    M1->run(BRAKE);
+    M2->run(BRAKE);
+    delay(10000);
+  }
+  Serial.println(dist_front);
+  
 }
--- a_GLOBAL/d_FUNCTIONS.ino
+++ a_GLOBAL/d_FUNCTIONS.ino
@@ -157,10 +157,10 @@ void follow_line(int speed, int angular, int timestep) {
   int line_sensor_3_value = digitalRead(line_sensor_3); 
 
   if(line_sensor_2_value==1 && line_sensor_3_value==0){
-    motor(speed, angular, timestep); 
+    motor(speed, -angular, timestep); 
   }
   else if(line_sensor_2_value==0 && line_sensor_3_value==1){
-    motor(speed, -angular, timestep);
+    motor(speed, angular, timestep);
   }
   else{
     motor(speed, 0, timestep); 
@@ -176,7 +176,7 @@ void nav_once() {
   if(line_sensor_1_value == 0 && line_sensor_4_value == 0) {
     //follow line
     Serial.println("Following line"); 
-    follow_line(250,50,200); 
+    follow_line(200,50,200); 
   }
   else {
     //at junctions
@@ -218,9 +218,12 @@ void nav_once() {
 
 void block_approach_line(){
   //Best activated when the block is closeset
-  double target_speed;
-  target_speed = target_speed-approach_PID.step(3 , ultrasound_front(true));
-  follow_line(target_speed,30,30);
+  double dist_front = ultrasound_front(1); 
+  if (dist_front<5.0){
+    M1->run(BRAKE);
+    M2->run(BRAKE);
+    delay(10000);
+  }
 }
 
 void follow_wall(double target_dist){
